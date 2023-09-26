fbee9045365a8a306e7f6ea0aeb94d172e45b7b9
RadioRek <nic.ortega@duocuc.cl>
2023-09-26 11:10:36 -0300

    xd 2

--- Sitio/views.py
+++ Sitio/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return render(request, "login.html")
 
 def home(request):
     return render(request, "home.html")
-
-def login(request):
-    return render(request, "login.html")
 
 def registro(request):
     return render(request, "registro.html")
